b56b973fd0c58001c3879f7604d44a8a3f046e5b
SolDavid_76 <david.janusz75@gmail.com>
2023-12-12 15:43:27 +0100

    some work

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -6,13 +6,15 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	touch /run/php/php7.3-fpm.pid
 	mkdir -p /var/www/html
 	cd /var/www/html
-	sudo -u www-data -i -- wp core download --allow-boot
+	#wp core download --path="/var/www/wordpress" --allow-root
+	#mv /var/www/wp-config.php /var/www/html/wordpress/wp-config.php
 	chown -R www-data:www-data /var/www/html
-	chown -R www-data:www-data /var/www/html/*
-	mv /var/www/wp-config.php /var/www/html/
+	chown -R www-data:www-data /var/www/*
 
-	sudo -u www-data -i -- wp core install --allow-root --url=${DOMAIN_NAME} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	sudo -u www-data -i -- wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --role=author --user_pass=${WP_USER_PASSWORD};
+	wp config create --path="/var/www/wordpress" --allow-root --dbname=${SQL_DATABASE} --dbuser=${SQL_USER} --dbpass=${SQL_PASSWORD} --dbhost=mariadb:3306 --url=https://${URL}
+	#wp core install --path="/var/www/wordpress" --allow-root --url=https://${URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	#wp user create --path="/var/www/wordpress" --allow-root ${WP_USR_LOGIN} ${WP_USR_EMAIL} --role=author --user_pass=${WP_USR_PASSWORD};
 
 fi
+
 exec "$@"
